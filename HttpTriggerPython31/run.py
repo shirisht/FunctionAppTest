9c10759ecc6b5023af1cfaf0cf995f34958bb0fa
Shirish Tatikonda <shirish.tatikonda@target.com>
2018-09-13 04:56:09 -0700

    removed world

--- HttpTriggerPython31/run.py
+++ HttpTriggerPython31/run.py
@@ -7,5 +7,5 @@ postreqdata = json.loads(open(os.environ['req']).read())
 print ("Python Version = '{0}'".format(platform.python_version()))
 
 response = open(os.environ['res'], 'w')
-response.write("hello world from "+postreqdata['name'])
+response.write("Hello from "+postreqdata['name'])
 response.close()
