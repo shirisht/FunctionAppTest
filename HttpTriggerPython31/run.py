8f2accabeee4362a712b8bb4835aa4aeb0dbdbe6
Shirish Tatikonda <shirish.tatikonda@target.com>
2018-09-13 05:26:19 -0700

    adding sentiment analyzer

--- HttpTriggerPython31/run.py
+++ HttpTriggerPython31/run.py
@@ -1,11 +1,27 @@
+import sys, os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'testenv/Lib/site-packages')))
+
 import os
 import json
 import platform
 
 postreqdata = json.loads(open(os.environ['req']).read())
 
-print ("Python Version = '{0}'".format(platform.python_version()))
+#print ("Python Version = '{0}'".format(platform.python_version()))
+
+#response = open(os.environ['res'], 'w')
+#response.write("Hello from "+postreqdata['name'])
+#response.close()
+
+import nltk
+import operator
+from nltk.sentiment import SentimentIntensityAnalyzer
+vader_analyzer = SentimentIntensityAnalyzer()
+scores = vader_analyzer.polarity_scores(postreqdata['text'])
+out = max(scores.items(), key=operator.itemgetter(1))[0]
 
 response = open(os.environ['res'], 'w')
-response.write("Hello from "+postreqdata['name'])
+response.write("Sentiment is "+ out)
 response.close()
+
+
